efee984c7d943a0f0ebd3026b2c665271f8ac376
tibawala <143086985+tibawala@users.noreply.github.com>
2023-10-19 00:41:44 -0700

    Add files via upload
    PYTHON TEST

---
+++ Question two .py
@@ -0,0 +1,12 @@
+  def number=(20,10):
+  x=20
+  y=10
+  print(20,10)
+  numbers()
+  def print(x,y)
+
+
+def my_numbers()
+  print(4,5,7)
+my_number()
+print((4,5,7))
---
+++ question three.py
@@ -0,0 +1,8 @@
+student_marks=[78,83,90,88,75]
+print(sun(student_marks))
+print(f"the sum of items in the student marks list is{sum(student_marks)}")
+
+
+print(student_marks[4]
+print student_marks[5]
+                
